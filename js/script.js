862db9f5b1ffe98cf7977d463815f901212fb6ed
Christian Chiarulli <chris.machine@pm.me>
2024-08-03 12:59:56 -0400

    detect download

--- js/script.js
+++ js/script.js
@@ -19,3 +19,33 @@ navLinks.forEach((link) => {
     knots_line.classList.toggle("pt-16");
   });
 });
+
+let os = "windows64";
+if (navigator.userAgent.indexOf("Mac") != -1) os = "mac";
+else if (navigator.userAgent.indexOf("Android") != -1) os = "android";
+else if (navigator.userAgent.indexOf("Linux") != -1) os = "linux64";
+else if (
+  navigator.userAgent.indexOf("WOW64") != -1 ||
+  navigator.userAgent.indexOf("Win64") != -1
+)
+  os = "windows64";
+// document.getElementById("other_downloads").classList.add("hidden");
+// document.getElementById("download_other").classList.remove("hidden");
+switch (os) {
+  case "android":
+    // Show default Windows + macOS
+    break;
+  case "windows64":
+    document.getElementById("download_macos").classList.add("hidden");
+    break;
+  case "linux64":
+    document.getElementById("download_win64").classList.add("hidden");
+    document.getElementById("download_macos").classList.add("hidden");
+    document.getElementById("download_signature").classList.add("hidden"); // n/a to PPA
+    document.getElementById("download_ubuntu").classList.remove("hidden");
+    document.getElementById("download_ubuntu").classList.add("flex");
+    break;
+  case "mac":
+    document.getElementById("download_win64").classList.add("hidden");
+    break;
+}
